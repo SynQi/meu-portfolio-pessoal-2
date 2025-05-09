6676b6f2d217d244ddf871df0e14f9d220f753c6
William Nunes <44782862+SynQi@users.noreply.github.com>
2025-05-09 02:52:35 -0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,10 +19,11 @@ const HeroSection: React.FC = () => {
       animate="visible"
     >
       <h1 className="text-5xl md:text-7xl font-bold text-gray-900 dark:text-white mb-4">
-        Olá, eu sou <span className="text-blue-600 dark:text-blue-400">Alex Silva</span>
+        Olá, eu sou <span className="text-blue-600 dark:text-blue-400">William Nunes
+        </span>
       </h1>
       <p className="text-xl md:text-2xl text-gray-700 dark:text-gray-300 mb-8 max-w-2xl">
-        Desenvolvedor Full-Stack Apaixonado por Criar Soluções Inovadoras e Experiências Digitais Incríveis.
+        Desenvolvedor e fascinado em segurança da informação, dedicado ao estudo e aprimoramento de minhas habilidades com Linux, Redes e Scripting.
       </p>
       <motion.button 
         className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-lg text-lg transition duration-300 ease-in-out transform hover:scale-105"
